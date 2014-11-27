af7999b229aa9bab33f65f487ff5d2b9e17b9d9c
Tyler Margison <kolorahl@gmail.com>
2014-11-26 20:10:37 -0800

    Do not assume Rails deployments use memcached
    The recipes, templates, etc for memcached should be in a separate cookbook, so
    that users may choose what caching backend they want. It is more common to see
    Redis anyway, so why someone would choose memcached as the default method of
    caching is odd.

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -26,22 +26,4 @@ node[:deploy].each do |application, deploy|
       deploy[:database][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
     end
   end
-
-  template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-    source "memcached.yml.erb"
-    cookbook 'rails'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(
-      :memcached => deploy[:memcached] || {},
-      :environment => deploy[:rails_env]
-    )
-
-    notifies :run, "execute[restart Rails app #{application}]"
-
-    only_if do
-      deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
-    end
-  end
 end
